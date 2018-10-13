5585fb02a022600eed0198660044fb8e867d362c
Eduardo Pereira <eduardo.pereira.santos@ccc.ufcg.edu.br>
2018-10-13 15:16:19 -0300

    Added method maior.

--- Route07/adt/linkedList/RecursiveSingleLinkedListImpl.java
+++ Route07/adt/linkedList/RecursiveSingleLinkedListImpl.java
@@ -1,6 +1,8 @@
 package adt.linkedList;
 
-public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
+import java.util.Arrays;
+
+public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T>, Comparable {
 
 	protected T data;
 	protected RecursiveSingleLinkedListImpl<T> next;
@@ -9,8 +11,19 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 
 	}
 
-	public RecursiveSingleLinkedListImpl(T data,
-			RecursiveSingleLinkedListImpl<T> next) {
+	public static void main(String[] args) {
+		RecursiveSingleLinkedListImpl<Integer> lista = new RecursiveSingleLinkedListImpl<Integer>();
+		lista.insert(10);
+		lista.insert(6);
+		lista.insert(8888);
+	
+		
+		System.out.println((lista.maior().getData()));
+
+
+	}
+
+	public RecursiveSingleLinkedListImpl(T data, RecursiveSingleLinkedListImpl<T> next) {
 		this.data = data;
 		this.next = next;
 	}
@@ -18,20 +31,19 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 	@Override
 	public boolean isEmpty() {
 		boolean isEmpty = false;
-		if(data == null) {
+		if (data == null) {
 			isEmpty = true;
 		}
 		return isEmpty;
-		
+
 	}
 
 	@Override
 	public int size() {
 		int tamanho;
-		if(isEmpty()) {
+		if (isEmpty()) {
 			tamanho = 0;
-		}
-		else {
+		} else {
 			tamanho = 1 + next.size();
 		}
 		return tamanho;
@@ -40,12 +52,12 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 	@Override
 	public T search(T element) {
 		T saida = null;
-		if(isEmpty()) {
-			
-		}else {
+		if (isEmpty()) {
+
+		} else {
 			if (this.getData().equals(element)) {
-				saida =   this.getData();
-			}else {
+				saida = this.getData();
+			} else {
 				saida = this.getNext().search(element);
 			}
 		}
@@ -54,28 +66,27 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 
 	@Override
 	public void insert(T element) {
-		if(element != null) {
-			
-			if(isEmpty()) {
+		if (element != null) {
+
+			if (isEmpty()) {
 				this.setData(element);
 				this.setNext(new RecursiveSingleLinkedListImpl<T>());
 			}
-			
-			
+
 			else {
 				this.getNext().insert(element);
 			}
-					
+
 		}
 	}
 
 	@Override
 	public void remove(T element) {
-		if(!isEmpty()) {
-			if(this.getData().equals(element)) {
+		if (!isEmpty()) {
+			if (this.getData().equals(element)) {
 				this.setData(this.getNext().getData());
 				this.setNext(this.getNext().getNext());
-			}else {
+			} else {
 				this.getNext().remove(element);
 			}
 		}
@@ -84,17 +95,17 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 	@Override
 	public T[] toArray() {
 		T[] result = (T[]) new Object[this.size()];
-		auxArray(result,this,0);
+		auxArray(result, this, 0);
 		return result;
 	}
 
-	public void auxArray(T[] array, RecursiveSingleLinkedListImpl<T> node,int i) {
-		if(!node.isEmpty()) {
+	public void auxArray(T[] array, RecursiveSingleLinkedListImpl<T> node, int i) {
+		if (!node.isEmpty()) {
 			array[i] = node.getData();
-			auxArray(array, node.getNext(),++i);
-			
+			auxArray(array, node.getNext(), ++i);
+
 		}
-		
+
 	}
 
 	public T getData() {
@@ -111,6 +122,47 @@ public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 
 	public void setNext(RecursiveSingleLinkedListImpl<T> next) {
 		this.next = next;
+	}
+
+	public RecursiveSingleLinkedListImpl<T> maior() {
+		RecursiveSingleLinkedListImpl<T> maior = this;
+		
+
+		if (this.next != null && !this.next.isEmpty()) {
+			maior = this;
+			RecursiveSingleLinkedListImpl<T> outroMaior = next.maior();
+			System.out.println(maior.getData());
+			System.out.println(outroMaior.getData());
+			maior = max(maior, outroMaior);
+			
+
+		}
+		return maior;
+
+	}
+
+	private RecursiveSingleLinkedListImpl<T> max(RecursiveSingleLinkedListImpl<T> maior,
+			RecursiveSingleLinkedListImpl<T> outroMaior) {
+		if (maior.compareTo(outroMaior) > 0) {
+			return maior;
+		} else if (outroMaior.compareTo(maior) > 0) {
+			return outroMaior;
+		} else {
+			return maior;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public int compareTo(Object o) {
+		
+		if ((Integer) ((RecursiveSingleLinkedListImpl<T>) o).getData() < (Integer) this.getData()) {
+			return 1;
+		}else if ( (Integer) ((RecursiveSingleLinkedListImpl<T>) o).getData() > (Integer) this.getData()) {
+			return -1;
+		}else {
+			return 0;
+		}
 	}
 
 }
